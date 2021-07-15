e35113a05f8d61cac5c4b36ea0cc68596a5aad11
Thunder <i@humg.top>
2021-07-15 11:01:37 +0800

    360

--- src/problem1/Main.java
+++ src/problem1/Main.java
@@ -24,20 +24,23 @@ public class Main {
         int[] add = {0, 100, 200, 360, 220};//添加及删除的开销，前面加个0是为了从1开始
         int[] del = {0, 120, 350, 200, 320};
 
-        //dp[i][j]表示从序号i到j变成回文串时的最小代价，而dp[i][j]可以由dp[i+1][j]和dp[i][j-1]得到
+        //dp[i][j]表示从序号i到j变成回文串时的最小代价， dp[i][j]可以由dp[i+1][j]和dp[i][j-1]得到
         //如果第i个字符和第j个字符不同，那么dp[i][j]可由dp[i+1][j]删除第i个字符，或者在字符串最末尾添加第i个字符得到，
         //也可以由dp[i][j-1]删除第j个字符，或者在最左侧添加第j个字符得到。
         for (int j = 1; j < str.length; j++) {
             for (int i = j - 1; i >= 0; i--) {
                 if (str[i] == str[j]) {
+                    //短回文转移到长回文
                     dp[i][j] = dp[i + 1][j - 1];
                 } else {
+                    //删除第i个字符，或者右侧添加第i个字符
                     int temp1 = Math.min(dp[i + 1][j] + del[str[i] - '0'],
                             dp[i + 1][j] + add[str[i] - '0']);
+                    //删除第j个字符，或者左侧添加第j个字符
                     int temp2 = Math.min(dp[i][j - 1] + del[str[j] - '0'],
                             dp[i][j - 1] + add[str[j] - '0']);
+                    //优化赋值
                     dp[i][j] = Math.min(temp1, temp2);
-
                 }
             }
         }
