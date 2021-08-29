0d673a70367cf76869735a0e3abdfa393968c883
NUC <me@humg.top>
2021-08-29 12:04:24 +0800

    笔试

--- src/problem1/Main.java
+++ src/problem1/Main.java
@@ -1,54 +1,55 @@
 package problem1;
 
 import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
 // 本地测试和牛客提交代码一致，无须修改相关
 public class Main {
-    static BufferedReader reader = getReader(); //初始化流
-    static BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    static Scanner reader = new Scanner(System.in);
 
 
     public static void main(String[] args) throws IOException {
+        int n = getNextInt();   //n本书
+        int[] thickness = getNextArr();
+        int[] width = getNextArr();
 
 
-        close();    //释放流资源
+        System.out.println(solution(n, thickness, width));
     }
 
+    private static long solution(int n, int[] thickness, int[] width) {
+        Arrays.sort(thickness);
+        Arrays.sort(width);
+        int cnt = 0;    //表示已经确定位置的元素数
+        long res = 1;
 
-//-------------------------------------------------以下为IO工具方法------------------------------------------------------------------
-
-
-    //打印数字
-    static void print(int num) throws IOException {
-        writer.write("" + num);
-    }
-
-    //打印字符串
-    static void print(String str) throws IOException {
-        writer.write(str);
-    }
-
-    static void newLine() throws IOException {
-        writer.newLine();
-    }
-
-    /**
-     * @return 返回扫描src目录下test.txt（存放用于测试的本地用例）的Reader
-     */
-    static BufferedReader getReader() {
-        try {
-            return new BufferedReader(new FileReader("src/problem1/test.txt"));
-        } catch (FileNotFoundException e) {
-            //代码粘贴到牛客里，使用的是System.in使用
-            return new BufferedReader(new InputStreamReader(System.in));
+        for (int i = n - 1; i >= 0; i--) {
+            int widthIdx = getWidthIdx(thickness[i], width);
+            int x = n - widthIdx - cnt;   //可放置位置
+            res *= x;
+            cnt++;
         }
+        return res % (1000000000 + 7);
+    }
+
+    //返回width数组中>=thick的最小元素的索引
+    static int getWidthIdx(int thick, int[] width) {
+        int idx = Arrays.binarySearch(width, thick);
+        if (idx < 0) {
+            return -(idx + 1);
+        }
+        while (idx > 0 && width[idx - 1] == thick) {
+            idx--;
+        }
+        return idx;
     }
 
     /**
      * @return 获取下一行的单个int数据
      */
     static int getNextInt() throws IOException {
-        return Integer.parseInt(reader.readLine());
+        return Integer.parseInt(reader.nextLine());
     }
 
     /**
@@ -57,21 +58,11 @@ public class Main {
      * @return 分割后的字符串转为的int数组
      */
     static int[] getNextArr() throws IOException {
-        String[] arr = reader.readLine().split(" ");
+        String[] arr = reader.nextLine().split(" ");
         int[] res = new int[arr.length];
         for (int i = 0; i < arr.length; i++) {
             res[i] = Integer.parseInt(arr[i]);
         }
         return res;
-    }
-
-    /**
-     * 释放流
-     *
-     * @throws IOException
-     */
-    static void close() throws IOException {
-        reader.close();
-        writer.close();
     }
 }
